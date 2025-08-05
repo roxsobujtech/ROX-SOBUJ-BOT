7d16a9ecc245c9ae2ab99a55394a4abbb02f58f2
ROX SOBUJ <roxsobujblog@gmail.com>
2025-08-05 17:42:13 +0600

    Update adduser.js

--- Script/commands/adduser.js
+++ Script/commands/adduser.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "adduser",
 	version: "2.4.3",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "☢️𖣘🆁🅾🆇 🆂🅾🅱🆄🅹☢️𖣘",
 	description: "Add user to the group by link or id",
 	commandCategory: "group",
 	usages: "[args]",
